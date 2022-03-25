d79e2ea22eb1c8966559e348a04f9dc49d61fc45
khusaini <@muhammadkhusaini599>
2022-03-25 10:45:16 +0700

    kelas dan objek

---
+++ src/main/kotlin/id/ac/polbeng/khusaini/test_kelas/Greeter.kt
@@ -0,0 +1,8 @@
+package id.ac.polbeng.khusaini.test_kelas
+
+class Greeter {
+    fun greet() {
+        println("Hello object world!")
+    }
+
+}
---
+++ src/main/kotlin/id/ac/polbeng/khusaini/test_kelas/GreeterTest.kt
@@ -0,0 +1,6 @@
+package id.ac.polbeng.khusaini.test_kelas
+
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+}

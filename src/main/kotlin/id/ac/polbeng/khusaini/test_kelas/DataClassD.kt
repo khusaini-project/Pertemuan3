f1b05c91315a19634a4423b095c3f7dd911eebde
khusaini <@muhammadkhusaini599>
2022-04-08 13:53:16 +0700

    Percobaan Data Class toString method

---
+++ src/main/kotlin/id/ac/polbeng/khusaini/test_kelas/DataClassD.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.khusaini.test_kelas
+
+//fungsi toString() mengembalikan nilai kembalian string
+fun main(){
+    val meri = Student("Meri", 20)
+    println(meri.toString()) //fungsi toString() untuk objek meri
+}

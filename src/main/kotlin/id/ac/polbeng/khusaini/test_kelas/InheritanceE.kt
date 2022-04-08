c43130fa1c4267ba223822b1a4305773b665f8bd
khusaini <@muhammadkhusaini599>
2022-04-08 10:51:03 +0700

    Percobaan Inheritance (Overriding Property's getter/setter method)

---
+++ src/main/kotlin/id/ac/polbeng/khusaini/test_kelas/InheritanceE.kt
@@ -0,0 +1,21 @@
+package id.ac.polbeng.khusaini.test_kelas
+
+open class NewPerson {
+    open var age: Int = 1
+}
+
+//child class
+class CheckedPerson: NewPerson() {
+    override var age: Int = 1
+        set(value) {
+            //value tidak boleh lebih kecil dari 0 atau value tidak boleh negatif
+            field = if(value > 0) value else throw IllegalArgumentException("Age can not be negative")
+        }
+}
+
+fun main() {
+    val person = NewPerson()
+    person.age = -5 //berhasil
+    val checkedPerson = CheckedPerson()
+    checkedPerson.age = -5 //Throws IllegalArgumentException : Age can not be negative
+}

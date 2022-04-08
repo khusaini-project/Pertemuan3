7a582f7c83f7dee66f1396bb89dd454477fbedd7
khusaini <@muhammadkhusaini599>
2022-04-08 11:26:19 +0700

    Percobaan Implimentasi Interface

---
+++ src/main/kotlin/id/ac/polbeng/khusaini/test_kelas/InterfaceA.kt
@@ -0,0 +1,28 @@
+package id.ac.polbeng.khusaini.test_kelas
+
+//implementasi interface
+interface MyInterface{
+    var str: String
+    fun demo()
+    fun func(){
+        println("Hello")
+    }
+}
+
+//kelas MyClass turunan dari kelas MyInterface
+class MyClass: MyInterface{
+    override var str: String = "Rekayasa Perangkat Lunak"
+    override fun demo() {
+        println("Demo Function")
+    }
+}
+
+fun main() {
+    //pemanggilan fungsi
+    val obj = MyClass()
+    obj.demo()
+    obj.func()
+
+    //tampilkan program
+    println(obj.str)
+}
